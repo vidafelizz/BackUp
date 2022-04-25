4d0403c01ec015595de250e46bd93276d8d7f0f2
vidafelizz <100111300+vidafelizz@users.noreply.github.com>
2022-04-25 22:40:10 +0900

    Add files via upload

--- ch02/Box.java
+++ ch02/Box.java
@@ -9,18 +9,15 @@ public class Box {
 		double width; //가로
 		double hight; //세로
 		double area; // 넓이
-		
 		double perimeter; //둘레값용
 		
 		
 		width = 10.0; // 값을 초기화(할당)
 		hight = 5.0;
 		area = width * hight; // 넓이 계산
-		
 		System.out.println("사각형의 넓이 : " + area); // 여기서의 "+"기호는 연결연산자로서 좌, 우의 문자 또는 값을 연결만 해준다.
 		
 		perimeter = 2.0*(width + hight);
-		
 		System.out.println("사각형의 둘레 : " + perimeter);
 		
 	}
