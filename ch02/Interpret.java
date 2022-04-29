a482aff9196c627e3512525dfdf475d907c39898
vidafelizz <100111300+vidafelizz@users.noreply.github.com>
2022-04-29 20:47:05 +0900

    Add files via upload

--- ch02/Interpret.java
+++ ch02/Interpret.java
@@ -11,7 +11,7 @@ public class Interpret {
 		b=2;
 		tot = a + b; 
 		//tot = tot + a + b; 반드시 지역변수는 초기화가 되어야만 계산이 된다.
-		//순차적 인터프리트 방식이기 떄문에 이런 현상이 나타난다.
+		//순차적 인터프리트 방식이기 때문에 이런 현상이 나타난다.
 		System.out.println(tot);
 
 	}
