c18f89769826f654badd301877eb4b66e6adb94b
vidafelizz <100111300+vidafelizz@users.noreply.github.com>
2022-04-30 16:42:01 +0900

    Add files via upload

--- homework/BreakEx4_1.java
+++ homework/BreakEx4_1.java
@@ -1,13 +1,13 @@
 package homework;
 
-public class BreakEx4 {
+public class BreakEx4_1 {
 	public static void main(String[] args) {
 	
 		int n=1;
-		while(n<8) {
+		while(true) {
+			System.out.println(n++ +" Hello World");
 			if(n==8) break;
-			System.out.println(n+" Hello World");
-			n++;		
+				
 		}
 		System.out.println("n값이 8이면 while문 빠져나옴");
 	}
