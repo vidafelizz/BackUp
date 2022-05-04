2eccbf4a169a8076162cb0fba59df3c5a9414057
vidafelizz <100111300+vidafelizz@users.noreply.github.com>
2022-05-04 14:35:16 +0900

    Add files via upload

--- ch05/QuizEx.java
+++ ch05/QuizEx.java
@@ -13,13 +13,14 @@ public class QuizEx {
 		for(int i=0; i<words.length; i++) {
 			System.out.printf("(문제%d) %s의 뜻은?", i+1, words[i][0]);
 			String tmp = scan.next(); // 문자처리
+
 			if(tmp.equals(words[i][1])) {
 				System.out.printf("정답입니다.%n%n");
 			} else {
 				System.out.printf("틀렸습니다. 정답은 %s 입니다. %n%n", words[i][1]);
 			}
+			System.out.println();
 		}
 		scan.close();
 	}
-
 }
